6b64dec08791fb16dba56d20e9e3ecf5c3892549
Shreyam Saha <sahashreyam123@gmail.com>
2021-06-28 15:06:20 +0530

    Added active driver location onto map

---
+++ lib/data/nearbyDriverData.dart
@@ -0,0 +1,28 @@
+import 'package:fast_aid/data-models/nearbyDrivers.dart';
+
+List<NearbyDriver> nearbyDriversList = [
+  NearbyDriver(
+      key: 'vyKYnb9FdbW08yniBOzHlQvGVbZ2',
+      latitude: 26.8564976,
+      longitude: 88.7261114),
+  NearbyDriver(
+      key: 'vyKYnb9FdbW08yniBOzHlQvGVbZ7',
+      latitude: 26.8564976,
+      longitude: 88.7251014),
+  NearbyDriver(
+      key: 'vyKYnb9FdbW08yniBOzHlQvGVbZ3',
+      latitude: 26.8654976,
+      longitude: 88.7361114),
+  NearbyDriver(
+      key: 'vyKYnb9FdbW08yniBOzHlQvGVbZ4',
+      latitude: 26.8665976,
+      longitude: 88.7262114),
+  NearbyDriver(
+      key: 'vyKYnb9FdbW08yniBOzHlQvGVbZ5',
+      latitude: 26.8664576,
+      longitude: 88.755996),
+  NearbyDriver(
+      key: 'vyKYnb9FdbW08yniBOzHlQvGVbZ1',
+      latitude: 26.8664976,
+      longitude: 88.7611140)
+];
--- lib/pages/Home-Page.dart
+++ lib/pages/Home-Page.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 import 'package:fast_aid/constants/Color-Constants.dart';
 import 'package:fast_aid/constants/Style-Constants.dart';
 import 'package:fast_aid/custom-widgets/Custom-Drawer.dart';
+import 'package:fast_aid/data-models/nearbyDrivers.dart';
+import 'package:fast_aid/data/nearbyDriverData.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_geofire/flutter_geofire.dart';
 import 'package:geolocator/geolocator.dart';
@@ -16,23 +18,26 @@ class _HomePageState extends State<HomePage> {
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   Completer<GoogleMapController> _controller = Completer();
   GoogleMapController mapController;
+  Set<Marker> _mapMarkers = {};
+  BitmapDescriptor driverIcon;
+
+  void createMarker() {
+    if (driverIcon == null) {
+      ImageConfiguration imageConfiguration =
+          createLocalImageConfiguration(context, size: Size(2, 2));
+      BitmapDescriptor.fromAssetImage(
+              imageConfiguration, 'assets/images/car_android.png')
+          .then((icon) {
+        driverIcon = icon;
+      });
+    }
+  }
 
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
   Position riderPosition;
-  Future<void> _goToUserPosition() async {
-    final GoogleMapController controller = await _controller.future;
-    Position currentPosition = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.bestForNavigation);
-    riderPosition = currentPosition;
-    LatLng pos = LatLng(currentPosition.latitude, currentPosition.longitude);
-    CameraPosition currentCP = new CameraPosition(target: pos, zoom: 14);
-    controller.animateCamera(CameraUpdate.newCameraPosition(currentCP));
-    //startGeofireListener();
-  }
-
   void getUserCurrentLocation() async {
     Position currentPosition = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.bestForNavigation);
@@ -41,26 +46,44 @@ class _HomePageState extends State<HomePage> {
     CameraPosition currentCP = new CameraPosition(target: pos, zoom: 14);
     mapController.animateCamera(CameraUpdate.newCameraPosition(currentCP));
     //startGeofireListener();
+    showNearbyDriverMarkers();
   }
 
   void startGeofireListener() {
-    print('HI out');
     Geofire.initialize('fast-aid-2392c-default-rtdb/data/driversAvailable');
     Geofire.queryAtLocation(riderPosition.latitude, riderPosition.longitude, 5)
         .listen((map) {
-      print('Hi');
       print(map);
     });
   }
 
+  void showNearbyDriverMarkers() {
+    Set<Marker> nearByDriverMarkers = new Set<Marker>();
+    for (NearbyDriver driver in nearbyDriversList) {
+      LatLng driverPos = LatLng(driver.latitude, driver.longitude);
+      Marker thisMarker = Marker(
+          markerId: MarkerId('driver${driver.key}'),
+          position: driverPos,
+          icon: BitmapDescriptor.defaultMarkerWithHue(
+              BitmapDescriptor.hueMagenta),
+          rotation: 0);
+      nearByDriverMarkers.add(thisMarker);
+      setState(() {
+        _mapMarkers = nearByDriverMarkers;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    createMarker();
     return Scaffold(
       key: _scaffoldKey,
       drawer: CustomDrawer(),
       body: Stack(
         children: <Widget>[
           GoogleMap(
+            markers: _mapMarkers,
             zoomControlsEnabled: false,
             initialCameraPosition: _kGooglePlex,
             mapType: MapType.normal,
@@ -90,7 +113,7 @@ class _HomePageState extends State<HomePage> {
           backgroundColor: kImperialRed,
           mini: true,
           onPressed: () {
-            _goToUserPosition();
+            getUserCurrentLocation();
           },
           child: Icon(Icons.my_location),
         ),
