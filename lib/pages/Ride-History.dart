00357abc1fbd60a305f8dc409d7171d2dcc21891
Shreyam Saha <sahashreyam123@gmail.com>
2021-06-29 17:25:16 +0530

    added circular progress

--- lib/pages/Ride-History.dart
+++ lib/pages/Ride-History.dart
@@ -2,12 +2,56 @@ import 'package:fast_aid/constants/Color-Constants.dart';
 import 'package:fast_aid/constants/Style-Constants.dart';
 import 'package:fast_aid/custom-widgets/Custom-Drawer.dart';
 import 'package:fast_aid/custom-widgets/Custom-List-Item.dart';
+import 'package:fast_aid/data-models/rideHistoryModel.dart';
 import 'package:fast_aid/data/userRideHistory.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class RideHistoryPage extends StatelessWidget {
+class RideHistoryPage extends StatefulWidget {
+  @override
+  _RideHistoryPageState createState() => _RideHistoryPageState();
+}
+
+class _RideHistoryPageState extends State<RideHistoryPage> {
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  bool isLoading = true;
+  void demoCircularIndicator() async {
+    await Future.delayed(Duration(seconds: 2));
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  Widget customLoader() {
+    demoCircularIndicator();
+    return Center(
+      child: CircularProgressIndicator(
+        backgroundColor: kImperialRed,
+        valueColor: AlwaysStoppedAnimation(kManatee),
+        strokeWidth: 5,
+      ),
+    );
+  }
+
+  Widget rideHistoryListBuilder() {
+    return ListView.builder(
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,
+      physics: BouncingScrollPhysics(),
+      itemCount: userRideHistoryList.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            decoration: kRideHistoryItemBoxDec,
+            child: CustomListItem(
+              rideData: userRideHistoryList[index],
+            ),
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,23 +69,7 @@ class RideHistoryPage extends StatelessWidget {
         ),
       ),
       drawer: CustomDrawer(),
-      body: ListView.builder(
-        scrollDirection: Axis.vertical,
-        shrinkWrap: true,
-        physics: BouncingScrollPhysics(),
-        itemCount: userRideHistoryList.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              decoration: kRideHistoryItemBoxDec,
-              child: CustomListItem(
-                rideData: userRideHistoryList[index],
-              ),
-            ),
-          );
-        },
-      ),
+      body: isLoading ? customLoader() : rideHistoryListBuilder(),
     );
   }
 }
